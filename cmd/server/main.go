dc622293309ded1b43d3a20bcc8281a494f5da9b
Gabriel Guesser <gdguesser@gmail.com>
2022-09-24 10:55:36 +0100

    refactoring folder structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	ggrpc "github.com/gdguesser/grpc-server/grpc"
 	"google.golang.org/grpc"
 )
+
+type Server struct {
+	ggrpc.UnimplementedGrpcServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *ggrpc.HelloRequest) (*ggrpc.HelloReply, error) {
+	return &ggrpc.HelloReply{Message: "Hello, " + in.GetName()}, nil
+}
 
 func main() {
 	println("gRPC server in Go")
@@ -17,7 +26,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	ggrpc.RegisterGrpcServer(s, &ggrpc.Server{})
+	ggrpc.RegisterGrpcServer(s, &Server{})
 	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
